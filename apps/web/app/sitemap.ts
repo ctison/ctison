e17f23bf6e2ef57194c69c911c6bbbf95844e163
Charles Tison <ctison@pm.me>
2023-12-06 03:25:19 +0100

    feat: /robots.txt & /sitemap.xml

---
+++ apps/web/app/sitemap.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next';
+import { links } from '@/_ui/links';
+import { allPosts } from 'contentlayer/generated';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const sitemapLinks: MetadataRoute.Sitemap = [];
+
+  links.forEach((link) => {
+    sitemapLinks.push({
+      url: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}${link.href}`,
+    });
+  });
+
+  allPosts.forEach((post) => {
+    sitemapLinks.push({
+      url: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}${post.url}`,
+    });
+  });
+
+  return sitemapLinks;
+}
