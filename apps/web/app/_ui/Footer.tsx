129eb6f7126c7281ec444b3f2e815db3f1973a5f
Charles Tison <ctison@pm.me>
2023-12-08 14:55:09 +0100

    chore: <Footer/> center

--- apps/web/app/_ui/Footer.tsx
+++ apps/web/app/_ui/Footer.tsx
@@ -1,8 +1,10 @@
 import {
   ActionIcon,
   Box,
+  Center,
   Container,
   Group,
+  Stack,
   Text,
   Tooltip,
 } from '@mantine/core';
@@ -11,7 +13,7 @@ import { DeBankMono } from 'react-web3-icons';
 
 export const Footer: React.FC = () => {
   return (
-    <Box
+    <Center
       component='footer'
       h='var(--app-footer-height)'
       style={{
@@ -22,23 +24,23 @@ export const Footer: React.FC = () => {
       pl='var(--_main-padding-left, calc(var(--app-shell-navbar-offset, 0px) + var(--app-shell-padding)))'
       pr='var(--_main-padding-right, calc(var(--app-shell-aside-offset, 0px) + var(--app-shell-padding)))'
     >
-      <Container h='100%' size='xs'>
-        <Group p='md' mah='100%' h='100%' maw='100%' justify='space-between'>
-          <Group gap='xs'>
-            <FooterIcon tooltip='Github' href='https://github.com/ctison'>
-              <VscGithub />
-            </FooterIcon>
-            <FooterIcon
-              tooltip='DeBank'
-              href='https://debank.com/profile/0x6ee4696cd792ec25a28b5cc1ba22b71fd2f3a1dd'
-            >
-              <DeBankMono />
-            </FooterIcon>
-          </Group>
-          <Text size='sm'>© {new Date().getFullYear()}</Text>
+      <Stack h='100%' mx='auto' gap={5} justify='center'>
+        <Group gap='xs'>
+          <FooterIcon tooltip='Github' href='https://github.com/ctison/ctison'>
+            <VscGithub />
+          </FooterIcon>
+          <FooterIcon
+            tooltip='DeBank'
+            href='https://debank.com/profile/0x6ee4696cd792ec25a28b5cc1ba22b71fd2f3a1dd'
+          >
+            <DeBankMono />
+          </FooterIcon>
         </Group>
-      </Container>
-    </Box>
+        <Text size='sm' ta='center'>
+          © {new Date().getFullYear()}
+        </Text>
+      </Stack>
+    </Center>
   );
 };
 
